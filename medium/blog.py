06d3ff48f24e5f4dc5cc8d6754d28d870c3a7ca3
dwivediom00 <dwivediom00.com>
2022-12-14 00:31:20 +0530

    ads view

--- medium/blog.py
+++ medium/blog.py
@@ -3,19 +3,20 @@ import tkinter.ttk as ttk
 from selenium import webdriver
 import time
 import threading
-driver = webdriver.Chrome(
-    executable_path="D:\\projects\\blog bot\\YoutubeBot\\edge\\chromedriver.exe")
+driver = webdriver.Chrome( executable_path="C:\\Users\\Administrator\\Desktop\\bogbot\\medium\\chromedriver.exe")
+# driver = webdriver.Chrome(executable_path="D:\\projects\\blog bot\\YoutubeBot\\edge\\chromedriver.exe")
 driver.get(
         'https://medium.com/@programmerspark21/how-to-choose-insurance-why-it-is-so-important-651900089cee')
 original_tab = driver.current_window_handle
+time.sleep(300)
 def run():
  try:
  
-   dur = 30
+   dur = 20
    
    i = 0
 
-   while i < 6:
+   while i < 1000000:
       driver.get(
         'https://medium.com/@programmerspark21/how-to-choose-insurance-why-it-is-so-important-651900089cee')
       original_tab = driver.current_window_handle
@@ -30,22 +31,44 @@ def run():
         "/html/body/div[1]/div/div/main/div/div[1]/div/div/article/div/div/h3/a")
       blogheading.click()
       driver.switch_to.window(original_tab)
+      
       driver.execute_script(
         "window.scrollTo(0,document.body.scrollHeight/2.5)")
-      time.sleep(dur)
+      ads1 = driver.find_element_by_id('ads1')
+      ads1.click()
+      time.sleep(15)
+      ads2 = driver.find_element_by_id('ads2')
+      ads2.click()
+      time.sleep(15)
+      ads3 = driver.find_element_by_id('ads3')
+      ads3.click()
+     
 
+      time.sleep(10)
+      
       backBtn = driver.find_element_by_xpath(
         "/html/body/div[1]/div/div/header/div/div[1]/a")
       backBtn.click()
       driver.switch_to.window(original_tab)
       driver.execute_script(
         "window.scrollTo(0,document.body.scrollHeight/2.5)")
+        
       time.sleep(4)
       latestblog = driver.find_element_by_xpath(
         "/html/body/div[1]/div/div/main/div/div[2]/div[1]/div[1]/div/div/h3/a")
       latestblog.click()
       driver.execute_script(
         "window.scrollTo(0,document.body.scrollHeight/2.5)")
+      time.sleep(6)
+      ads1 = driver.find_element_by_id('ads1')
+      ads1.click()
+      time.sleep(15)
+      ads2 = driver.find_element_by_id('ads2')
+      ads2.click()
+      time.sleep(15)
+      ads3 = driver.find_element_by_id('ads3')
+      ads3.click()
+      time.sleep(15)
 
     #  print("its working ")
       time.sleep(dur)
