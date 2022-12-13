361d414971515da572269bc9e3f9fd1f209f1d13
dwivediom00 <dwivediom00.com>
2022-12-13 22:10:21 +0530

    recursion is on when error

--- linkdin/blog.py
+++ linkdin/blog.py
@@ -3,43 +3,55 @@ import tkinter.ttk as ttk
 from selenium import webdriver
 import time
 import threading
-dur =30 
-driver = webdriver.Chrome(executable_path="D:\\projects\\blog bot\\YoutubeBot\\edge\\chromedriver.exe")
-i = 0
-  
-while i < 6:
-    driver.get('https://www.linkedin.com/posts/programmers-park-52129225a_check-out-my-new-blog-about-insurance-activity-7008390675704500224-Lhca?utm_source=share&utm_medium=member_desktop')
+driver = webdriver.Chrome(
+    executable_path="D:\\projects\\blog bot\\YoutubeBot\\edge\\chromedriver.exe")
+driver.get('https://www.linkedin.com/posts/programmers-park-52129225a_check-out-my-new-blog-about-insurance-activity-7008390675704500224-Lhca?utm_source=share&utm_medium=member_desktop')
+original_tab = driver.current_window_handle
+def run():
+ try:
+ 
+   dur = 30
+   
+   i = 0
 
-    time.sleep(4)
-    link = driver.find_element_by_xpath(
+   while i < 6:
+      driver.get('https://www.linkedin.com/posts/programmers-park-52129225a_check-out-my-new-blog-about-insurance-activity-7008390675704500224-Lhca?utm_source=share&utm_medium=member_desktop')
+      driver.switch_to.window(original_tab)
+      time.sleep(4)
+      link = driver.find_element_by_xpath(
         "/html/body/main/section[1]/div/section/div/p/a")
 
-    driver.execute_script(
+      driver.execute_script(
         "arguments[0].setAttribute('target', '_self')", link)
-    link.click()
-
-    blogheading = driver.find_element_by_xpath(
+      link.click()
+      
+      blogheading = driver.find_element_by_xpath(
         "/html/body/div[1]/div/div/main/div/div[1]/div/div/article/div/div/h3/a")
-    blogheading.click()
-
-    driver.execute_script(
+      blogheading.click()
+      driver.switch_to.window(original_tab)
+      driver.execute_script(
         "window.scrollTo(0,document.body.scrollHeight/2.5)")
-    time.sleep(dur)
+      time.sleep(dur)
 
-    backBtn = driver.find_element_by_xpath(
+      backBtn = driver.find_element_by_xpath(
         "/html/body/div[1]/div/div/header/div/div[1]/a")
-    backBtn.click()
-    driver.execute_script(
+      backBtn.click()
+      driver.switch_to.window(original_tab)
+      driver.execute_script(
         "window.scrollTo(0,document.body.scrollHeight/2.5)")
-    time.sleep(4)
-    latestblog = driver.find_element_by_xpath(
+      time.sleep(4)
+      latestblog = driver.find_element_by_xpath(
         "/html/body/div[1]/div/div/main/div/div[2]/div[1]/div[1]/div/div/h3/a")
-    latestblog.click()
-    driver.execute_script(
+      latestblog.click()
+      driver.execute_script(
         "window.scrollTo(0,document.body.scrollHeight/2.5)")
 
     #  print("its working ")
-    time.sleep(dur)
-    driver.refresh()
+      time.sleep(dur)
+      driver.refresh()
 
-    i += 1
+      i += 1
+ except:
+    print("an error occord")
+    run()
+run()    
--- medium/blog.py
+++ medium/blog.py
@@ -3,44 +3,56 @@ import tkinter.ttk as ttk
 from selenium import webdriver
 import time
 import threading
-
-driver = webdriver.Chrome( executable_path="D:\\projects\\blog bot\\YoutubeBot\\edge\\chromedriver.exe")
-dur=30 
-i = 0
-while i < 6:
-    driver.get(
+driver = webdriver.Chrome(
+    executable_path="D:\\projects\\blog bot\\YoutubeBot\\edge\\chromedriver.exe")
+driver.get(
         'https://medium.com/@programmerspark21/how-to-choose-insurance-why-it-is-so-important-651900089cee')
+original_tab = driver.current_window_handle
+def run():
+ try:
+ 
+   dur = 30
+   
+   i = 0
 
-    time.sleep(4)
-    link = driver.find_element_by_xpath(
-        "/html/body/div/div/div[3]/div[2]/div/main/div/div[2]/div[1]/div/article/div/div[2]/section/div/div[2]/p[8]/a")
+   while i < 6:
+      driver.get(
+        'https://medium.com/@programmerspark21/how-to-choose-insurance-why-it-is-so-important-651900089cee')
+      original_tab = driver.current_window_handle
+      time.sleep(4)
+      link = driver.find_element_by_xpath("/html/body/div/div/div[3]/div[2]/div/main/div/div[2]/div[1]/div/article/div/div[2]/section/div/div[2]/p[8]/a")
 
-    driver.execute_script(
+      driver.execute_script(
         "arguments[0].setAttribute('target', '_self')", link)
-    link.click()
-
-    blogheading = driver.find_element_by_xpath(
+      link.click()
+      
+      blogheading = driver.find_element_by_xpath(
         "/html/body/div[1]/div/div/main/div/div[1]/div/div/article/div/div/h3/a")
-    blogheading.click()
-
-    driver.execute_script(
+      blogheading.click()
+      driver.switch_to.window(original_tab)
+      driver.execute_script(
         "window.scrollTo(0,document.body.scrollHeight/2.5)")
-    time.sleep(dur)
+      time.sleep(dur)
 
-    backBtn = driver.find_element_by_xpath(
+      backBtn = driver.find_element_by_xpath(
         "/html/body/div[1]/div/div/header/div/div[1]/a")
-    backBtn.click()
-    driver.execute_script(
+      backBtn.click()
+      driver.switch_to.window(original_tab)
+      driver.execute_script(
         "window.scrollTo(0,document.body.scrollHeight/2.5)")
-    time.sleep(4)
-    latestblog = driver.find_element_by_xpath(
+      time.sleep(4)
+      latestblog = driver.find_element_by_xpath(
         "/html/body/div[1]/div/div/main/div/div[2]/div[1]/div[1]/div/div/h3/a")
-    latestblog.click()
-    driver.execute_script(
+      latestblog.click()
+      driver.execute_script(
         "window.scrollTo(0,document.body.scrollHeight/2.5)")
 
     #  print("its working ")
-    time.sleep(dur)
-    driver.refresh()
+      time.sleep(dur)
+      driver.refresh()
 
-    i += 1
+      i += 1
+ except:
+    print("an error occord")
+    run()
+run()    
